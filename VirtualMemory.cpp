105437006f0077d18135d04a76ca5e6ca528f784
shiras <shira.schattner@mail.huji.ac.il>
2023-06-18 13:15:46 +0300

    .

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -5,17 +5,27 @@
 #define PAGE_TABLE_WIDTH OFFSET_WIDTH
 #define PAGE_TABLE_BITS (1 << PAGE_TABLE_WIDTH) - 1;
 
-/*H
+uint64_t CalcPhysicalAddress (uint64_t frameAddress, uint64_t lineIndex)
+{
+  return (frameAddress * PAGE_SIZE) + lineIndex;
+}
+
+void InitializeFrame (uint64_t frameNum)
+{
+  for (uint64_t i = 0; i < PAGE_SIZE; i++)
+  {
+    //TODO Maybe don't initialize it to 0
+    uint64_t physicalAddress = CalcPhysicalAddress (frameNum, i);
+    PMwrite (physicalAddress, 0);
+  }
+}
+
+/*
  * Initialize the virtual memory.
  */
 void VMinitialize ()
 {
-
-  for (uint64_t i = 0; i < PAGE_SIZE; i++)
-  {
-    //TODO Maybe don't initialize it to 0
-    PMwrite (i, 0);
-  }
+  InitializeFrame (0);
 }
 
 //TODO CHECK BITSHIFT HELPER FUNCTIONS
@@ -32,8 +42,8 @@ uint64_t UpdateVirtualAddress (uint64_t virtualAddress)
 
 void MaxBFS (uint64_t frameNum, uint64_t *max, int iter)
 {
-
-  if(iter == TABLES_DEPTH) { return; }
+  if (iter == TABLES_DEPTH)
+  { return; }
 
   word_t tmpValue;
 
@@ -44,39 +54,218 @@ void MaxBFS (uint64_t frameNum, uint64_t *max, int iter)
     if (tmpValue != 0)
     {
 
-      if((uint64_t)tmpValue > *max) {
+      if ((uint64_t) tmpValue > *max)
+      {
         *max = tmpValue;
       }
 
-      MaxBFS (tmpValue, max, iter+1);
+      MaxDFS (tmpValue, max, iter + 1);
+    }
+  }
+}
+
+uint64_t ZeroTableDFS (uint64_t frameNum, uint64_t prevFrame, uint64_t
+indexInPrevFrame, int iter, uint64_t dontTake)
+{
+  if (iter == TABLES_DEPTH)
+  { return 0; }
+
+  word_t tmpValue;
+  uint64_t tmpReturn;
+  bool allZero = true;
+
+  for (uint64_t j = 0; j < PAGE_SIZE; j++)
+  {
+    PMread (CalcPhysicalAddress (frameNum, j), &tmpValue);
+
+    if (tmpValue != 0)
+    {
+      allZero = false;
+      tmpReturn = ZeroTableDFS (tmpValue, frameNum, j, iter + 1, dontTake);
+
+      if (tmpReturn != 0)
+      {
+        return tmpReturn;
+      }
     }
   }
 
+  if (allZero && (prevFrame != -1) && (frameNum != dontTake))
+  {
+    PMwrite (CalcPhysicalAddress (prevFrame, indexInPrevFrame), 0);
+    return frameNum;
+  }
+
+  return 0;
+
+}
+
+uint64_t GetMinValue (uint64_t a, uint64_t b)
+{
+  if (a < b)
+  {
+    return a;
+  }
+  return b;
+}
+
+uint64_t GetAbsulutValue (uint64_t num)
+{
+  if (num >= 0)
+  {
+    return num;
+  }
+  return -1 * num;
+}
+
+void SwapDFS (uint64_t pageSwappedIn, uint64_t pageSwappedOut,
+              uint64_t *frameSwappedOut, uint64_t frameNum, uint64_t *max,
+              int iter, uint64_t parentFrame, uint64_t *parentFrameFinal,
+              uint64_t *evictedPageIndex)
+{
+  if (iter == TABLES_DEPTH)
+  {
+    // we reached a frame that contains a page (and not a page table)
+    uint64_t cyclicalDistance =
+        GetMinValue (
+            NUM_PAGES - GetAbsulutValue (pageSwappedIn - pageSwappedOut),
+            GetAbsulutValue (pageSwappedIn - pageSwappedOut));
+
+    if (cyclicalDistance > *max)
+    {
+      *max = cyclicalDistance;
+      *frameSwappedOut = frameNum;
+      *parentFrameFinal = parentFrame;
+      *evictedPageIndex = pageSwappedOut;
+    }
+
+    return;
+  }
+
+  word_t tmpValue;
+
+  for (uint64_t j = 0; j < PAGE_SIZE; j++)
+  {
+    parentFrame = frameNum;
+    PMread (frameNum * PAGE_SIZE + j, &tmpValue);
+
+    pageSwappedOut = (pageSwappedOut << OFFSET_WIDTH) + j;
+
+    if (tmpValue != 0)
+    {
+      SwapDFS (pageSwappedIn, pageSwappedOut, frameSwappedOut, tmpValue, max,
+               iter + 1, parentFrame, parentFrameFinal, evictedPageIndex);
+    }
+  }
+}
+
+uint64_t getFreeFrame (uint64_t pageNum, uint64_t dontTake)
+{
+  uint64_t frameNumber = ZeroTableDFS (0, -1, -1, 0, dontTake);
+  if (frameNumber != 0)
+  {
+    return frameNumber;
+  }
+
+  frameNumber = 0;
+  MaxDFS (0, &frameNumber, 0);
+  if (frameNumber + 1 < NUM_FRAMES)
+  {
+    return frameNumber + 1;
+  }
+
+  frameNumber = 0;
+  uint64_t parentFrame = 0;
+  uint64_t max = 0;
+  uint64_t evictedPageIndex = 0;
+  SwapDFS (pageNum, 0, &frameNumber, 0, &max, 0, 0, &parentFrame, &evictedPageIndex);
+  word_t tmp;
+  for (int i = 0; i < PAGE_SIZE; i++)
+  {
+    uint64_t physicalAddress = CalcPhysicalAddress (parentFrame, i);
+    PMread (physicalAddress, &tmp);
+    if (tmp == frameNumber)
+    {
+      PMwrite (physicalAddress, 0);
+      break;
+    }
+  }
+
+  PMevict (frameNumber, evictedPageIndex);
+  return frameNumber;
 }
 
 uint64_t GetTargetPhysicalAddress (uint64_t virtualAddress)
 {
+  /*
+   * 1. for TABLE_DEPTH iterations:
+   *    a.  the next page table we want to reach is located in the left
+   *        PAGE_TABLE_WIDTH bits of the current virtual address.
+   *        We need to extract this index.
+   *    b.  Go to this index in the frame that matches the current page
+   *        table and read the value.
+   *    c.  if the value is 0:
+   *            i.  it means the next page table doesn't exist yet, we need
+   *                to create it (separate function).
+   *                This function should find an empty frame (or evict one
+   *                if all frames are full), initialize it to 0 and return
+   *                the frame number.
+   *            ii. write the returned frame number to the next page's line
+   *                in the frame that matches the current page table.
+   *        otherwise:
+   *            i.  the next page table is stored in the frame with the
+   *                index that matches the read value.
+   * 2. When done, return the last frame index that was calculated.
+   */
+
   // TODO: check validity of the virtual address somehow and return -1 if
   //  invalid
-  uint64_t frameAddress = 0;
+  uint64_t frameNum = 0;
   word_t tmpValue;
+  uint64_t prevFrameNum = -1;
 
   for (uint64_t i = 0; i <= TABLES_DEPTH; i++)
   {
     uint64_t lineIndex = CalculateLineIndex (virtualAddress);
     uint64_t physicalAddress = frameAddress + lineIndex;
     PMread (physicalAddress, &tmpValue);
-    frameAddress = tmpValue * PAGE_SIZE;
+    frameNum = tmpValue;
 
     if ((i != TABLES_DEPTH) && (frameAddress == 0))
     {
       //TODO DEAL WITH CASE THAT FRAME ADDRESS IS 0
     }
 
-    virtualAddress = UpdateVirtualAddress (virtualAddress);
+    prevFrameNum = frameNum;
+
+    pageNum = UpdatePageNum (pageNum);
   }
 
-  return frameAddress;
+  return frameNum;
+}
+
+uint64_t PrepareForReadWrite (uint64_t virtualAddress)
+{
+  /*
+ * 1. remove the offset from virtualAddress to get the target page number.
+ * 2. get the offset from virtualAddress to get the target word index of
+ *    the target page.
+ * 3. find the target page by traversing the tree and creating tables if
+ *    needed.
+ *    This should be a separate function that returns the empty frame number
+ *    in the physical memory where we will insert the page's data.
+ * 4. call PMRestore on the frame number from (3) and page number from (1)
+ * 5. create the target physical address of the word in the frame by
+ *    adding the offset from (2) to the frame number from (3).
+ */
+  uint64_t pageNumber = RemoveOffset (virtualAddress);
+  uint64_t wordIndex = GetOffset (virtualAddress);
+  uint64_t targetFrame = GetTargetFrame (pageNumber);
+
+  PMrestore (targetFrame, pageNumber);
+
+  uint64_t targetPhysicalAddress = CalcPhysicalAddress (targetFrame, wordIndex);
+  return targetPhysicalAddress;
 }
 
 /* Reads a word from the given virtual address
@@ -88,13 +277,15 @@ uint64_t GetTargetPhysicalAddress (uint64_t virtualAddress)
  */
 int VMread (uint64_t virtualAddress, word_t *value)
 {
-  uint64_t physicalAddress = GetTargetPhysicalAddress (virtualAddress);
-  if (physicalAddress == -1)
+  uint64_t targetPhysicalAddress = PrepareForReadWrite (virtualAddress);
+
+  if (targetPhysicalAddress == -1)
   {
     return 0;
   }
 
-  PMread (physicalAddress, value);
+  PMread (targetPhysicalAddress, value);
+
   return 1;
 }
 
@@ -106,15 +297,14 @@ int VMread (uint64_t virtualAddress, word_t *value)
  */
 int VMwrite (uint64_t virtualAddress, word_t value)
 {
-  uint64_t physicalAddress = GetTargetPhysicalAddress (virtualAddress);
-  if (physicalAddress == -1)
+  uint64_t targetPhysicalAddress = PrepareForReadWrite (virtualAddress);
+
+  if (targetPhysicalAddress == -1)
   {
     return 0;
   }
 
-  uint64_t frameIndex = physicalAddress / PAGE_SIZE;
-  uint64_t pageIndex = CalcultePageIndex(virtualAddress); // remove the offset
-  PMrestore (frameIndex, pageIndex);
-  PMwrite (physicalAddress, value);
+  PMwrite (targetPhysicalAddress, value);
+
   return 1;
 }
